d3512d80a67cb3568e7147c1073a216dc9eb98bb
KaanGullum <163915166+KaanGullum@users.noreply.github.com>
2026-03-31 22:39:03 +0300

    Initial Commit

---
+++ MealMap/MealMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMapApp.swift
+//  MealMap
+//
+//  Created by Kaan Güllü on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MealMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
